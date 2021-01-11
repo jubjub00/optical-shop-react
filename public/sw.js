8480f7eeff0bd9177c8d7e6e2eb08a5afd83d62a
jubjub00 <hhabagu@gmail.com>
2021-01-11 15:44:52 +0700

    add servce worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,36 @@
-console.warn("sw is public file")
+const cacheName = "optical-shop-asset";
+const staticAssets = [
+  "./",
+  "./index.html",
+  "./scripts/app.js",
+  "./styles/style.css",
+  "./favicon.ico",
+];
+
+// Start the service worker and cache all of the app's content
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(staticAssets);
+    })
+  );
+});
+
+// Serve cached content when offline
+// https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent/request
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request)
+        .then(function (response) {
+          return response;
+        })
+        .catch(function (error) {
+          throw error;
+        });
+    })
+  );
+});
