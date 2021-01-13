74e48507be7b52d7e9d2cefd251e6a68e6d9d66f
jubjub00 <hhabagu@gmail.com>
2021-01-14 00:06:55 +0700

    add navigation drawer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         sdfasdfasdf
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+function HomeScreen({ navigation }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        sdfasdfasdf
-      </header>
-    </div>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
   );
 }
 
-export default App;
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
